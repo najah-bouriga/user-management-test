ca4e000606efd02e4ab4dda21d6ead5d31c45aa6
najahbouriga <najahbouriga64@gmail.com>
2024-11-24 23:09:54 +0100

    add pagination

--- users/views.py
+++ users/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
 from .models import User
 from .serializers import UserSerializer
 
+
+class UserPagination(PageNumberPagination):
+    page_size = 1
+    page_size_query_param = 'page_size'  # Allow clients to define page size
+    max_page_size = 100  # Max items per page
+
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    pagination_class = UserPagination  # Apply custom pagination to this viewset
